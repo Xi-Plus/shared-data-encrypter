c7c3702086691a46be88f9876ec2ba56fc141691
xiplus <huangxuanyuxiplus@gmail.com>
2021-06-05 11:29:27 +0800

    Update test_basic.cpp

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -1,13 +1,34 @@
 #include "sde.hpp"
 
 int main() {
-	SDE::Data apple = SDE::Data("secret for apple");
-	SDE::Data banana = SDE::Data("secret for banana");
-	SDE::DataAccess alice_apple = SDE::DataAccess();
-	SDE::DataAccess alice_banana = SDE::DataAccess();
-	SDE::DataAccess bob_banana = SDE::DataAccess();
+	std::string apple_str = "secret for apple";
+	std::string banana_str = "secret for apple";
 
+	std::string alice_pass = "password for alice";
+	std::string bob_pass = "password for bob";
+
+	// Create data apple
+	SDE::Data apple = SDE::Data(apple_str);
+
+	// Give access to alice for apple
+	SDE::DataAccess alice_apple = SDE::DataAccess(alice_pass);
 	apple.giveAccessTo(alice_apple);
+
+	// apple.encryptData();
+	// alice_apple.encryptDataKey();
+
+	// Create data banana
+	SDE::Data banana = SDE::Data(banana_str);
+
+	// Give access to alice for banana
+	SDE::DataAccess alice_banana = SDE::DataAccess(alice_pass);
 	banana.giveAccessTo(alice_banana);
+
+	// Give access to bob for banana
+	SDE::DataAccess bob_banana = SDE::DataAccess(bob_pass);
 	banana.giveAccessTo(bob_banana);
+
+	// banana.encryptData();
+	// alice_banana.encryptDataKey();
+	// bob_banana.encryptDataKey();
 }
