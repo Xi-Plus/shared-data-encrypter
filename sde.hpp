cee86f87ca5ef9cd5f3b7755e830e29346836055
xiplus <huangxuanyuxiplus@gmail.com>
2021-06-06 19:20:09 +0800

    Add more comments

--- sde.hpp
+++ sde.hpp
@@ -13,6 +13,9 @@ namespace SDE {
  */
 class RSAEncrypter {
    public:
+	/**
+	 * Create a new RSAEncrypter object.
+	 */
 	RSAEncrypter();
 
 	/**
@@ -81,11 +84,28 @@ class RSAEncrypter {
  */
 class AESEncrypter {
    public:
+	/**
+	 * Create a new AESEncrypter object.
+	 */
 	AESEncrypter(std::string password);
 
+	/**
+	 * Encrypt a string.
+	 *
+	 * @param plainText Plain text to be encrypted.
+	 */
 	std::string encryptString(std::string plainText);
+
+	/**
+	 * Decrypt a string. No exceptions raised when it failed.
+	 *
+	 * @param encrypted Encrypted text to be decrypted.
+	 */
 	std::string decryptString(std::string encrypted);
 
+	/**
+	 * Generate a key (password) to be used in AESEncrypter.
+	 */
 	static std::string GeneratePassword();
 
    private:
@@ -97,14 +117,55 @@ class AESEncrypter {
  */
 class DataAccess {
    public:
+	/**
+	 * Create a new DataAccess object.
+	 *
+	 * @param password Used for AESEncrypter.
+	 */
 	DataAccess(std::string password);
+
+	/**
+	 * Restore DataAccess object from old data.
+	 *
+	 * @param _encodedUserPublicKey Base64-encoded DataAccess public key.
+	 * @param _encryptedUserPrivateKey Encrypted DataAccess private key.
+	 * @param _encryptedDataKeyEncrypted Encrypted Data key.
+	 */
 	DataAccess(std::string _encodedUserPublicKey, std::string _encryptedUserPrivateKey, std::string _encryptedDataKey);
 
+	/**
+	 * Encrypt the Data key. It generate encryptedDataKey and destroy dataKey, then lock the access.
+	 */
 	void encryptDataKey();
+
+	/**
+	 * Decrypt the Data key. It restores dataKey from encryptedDataKey, then unlock the access.
+	 *
+	 * @param password Used for AESEncrypter to decrypt.
+	 */
 	void decryptDataKey(std::string password);
+
+	/**
+	 * Decrypt the dataKey then encrypt it with new password.
+	 *
+	 * @param oldPassword Old password for AESEncrypter to decrypt.
+	 * @param newPassword New password for AESEncrypter to encrypt.
+	 */
 	void changePassword(std::string oldPassword, std::string newPassword);
+
+	/**
+	 * Get DataAccess public key.
+	 */
 	std::string getUserPublicKey();
+
+	/**
+	 * Get encrypted DataAccess private key.
+	 */
 	std::string getEncryptedUserPrivateKey();
+
+	/**
+	 * Get encrypted Data key.
+	 */
 	std::string getEncryptedDataKey();
 
    private:
@@ -125,13 +186,50 @@ class DataAccess {
  */
 class Data {
    public:
+	/**
+	 * Create a new Data with plain text.
+	 *
+	 * @param _data Plain text to encrypt.
+	 */
 	static Data newFromPlain(std::string _data);
+
+	/**
+	 * Restore old Data with encrypted text.
+	 *
+	 * @param _encryptedData Encrypted text to decrypt.
+	 */
 	static Data newFromEncrypted(std::string _encryptedData);
 
+	/**
+	 * Encrypt the string. It generate encryptedData and destroy plain data, then lock the data.
+	 */
 	void encryptData();
+
+	/**
+	 * Decrypt the string. It restores plain data from encryptedData, then unlock the data.
+	 *
+	 * @param access Used for RSAEncrypter to decrypt.
+	 */
 	void decryptData(DataAccess& access);
+
+	/**
+	 * Assign acess permission to DataAcess.
+	 *
+	 * @param access Who needs the permission.
+	 * @exception std::runtime_error The data is locked.
+	 */
 	void giveAccessTo(DataAccess& access);
+
+	/**
+	 * Get plain text.
+	 *
+	 * @exception std::runtime_error The data is locked.
+	 */
 	std::string getData();
+
+	/**
+	 * Get encrypted string.
+	 */
 	std::string getEncryptedData();
 
    private:
