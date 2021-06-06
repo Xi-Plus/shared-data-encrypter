b23a6a7f358e0890ad3ded6619756e5a3570e7ca
docs_bot <docs_bot@github.com>
2021-06-06 11:26:18 +0000

    Build docs

--- docs/search/all_7.js
+++ docs/search/all_7.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['setencodedprivatekey_24',['setEncodedPrivateKey',['../classSDE_1_1RSAEncrypter.html#a93fded8f1b8704b144f62c75b1f1dc99',1,'SDE::RSAEncrypter']]],
   ['setencodedpublickey_25',['setEncodedPublicKey',['../classSDE_1_1RSAEncrypter.html#ac66a82429dfa12004d899a2194c1f23e',1,'SDE::RSAEncrypter']]],
-  ['shared_2ddata_2dencrypter_26',['shared-data-encrypter',['../md_README.html',1,'']]]
+  ['shared_2ddata_2dencrypter_26',['shared-data-encrypter',['../index.html',1,'']]]
 ];
